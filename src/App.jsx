5d789659ef89c927018402c57e91da993ce9019d
Joaquin Ginzo <joaco@joaquins-air.home>
2021-09-21 16:30:28 +0100

    Icons added

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import logo from './logo.svg';
 import './App.scss';
 import Example from './pages/example/example.jsx';
+import Navbar from './components/navbar/navbar';
 
 const  App = () => {
   return (
     <div className="App">
+      <Navbar/>
     </div>
   );
 }
--- src/components/navbar/navbar.jsx
+++ src/components/navbar/navbar.jsx
@@ -1,31 +1,38 @@
 import React from 'react'
 import "./navbar.scss";
+import "../../assets/styles/_variables.scss"
+import book from "../../assets/icons/book.svg"
+import medals from "../../assets/icons/medals.svg"
+import nut from "../../assets/icons/nut.svg"
+import students from "../../assets/students.svg"
 
-const navbar = () => {
+
+const Navbar = () => {
     return (
-        <div className="navbar">
-            <h1>_n</h1>
-            <div>
-                <p>Icon</p>
+        <nav className="navbar">
+            <h1 className="navbar_item">_n</h1>
+            <div className="navbar__item">
+                <img src={book} alt="" />
                 <p>Dashboard</p>
             </div>
-            <div>
-
+            <div className="navbar__item">
+                <p>Icon</p>
+                <p>Courses</p>
             </div>
-            <div>
-
+            <div className="navbar__item">
+                <p>Icon</p>
+                <p>Students</p>
             </div>
-            <div>
-
+            <div className="navbar__item">
+                <p>Icon</p>
+                <p>Enrollment</p>
             </div>
-            <div>
-
-            </div>
-
-            
-            
-        </div>
+            <div className="navbar__item">
+                <p>Icon</p>
+                <p>Settings</p>
+            </div>     
+        </nav>
     )
 }
 
-export default navbar
+export default Navbar
