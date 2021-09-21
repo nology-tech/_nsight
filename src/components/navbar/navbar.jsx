6174d7e194154de5d0c2b17cad757530448d1cb6
Joaquin Ginzo <joaco@joaquins-air.home>
2021-09-21 15:58:48 +0100

    new folder structure

--- src/components/navbar/navbar.jsx
+++ src/components/navbar/navbar.jsx
@@ -4,6 +4,25 @@ import "./navbar.scss";
 const navbar = () => {
     return (
         <div className="navbar">
+            <h1>_n</h1>
+            <div>
+                <p>Icon</p>
+                <p>Dashboard</p>
+            </div>
+            <div>
+
+            </div>
+            <div>
+
+            </div>
+            <div>
+
+            </div>
+            <div>
+
+            </div>
+
+            
             
         </div>
     )
