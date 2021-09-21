253bc1634ca5416e2421f3dde9e034e20c042ac2
khorh <helen.khor@googlemail.com>
2021-09-21 16:57:32 +0100

    outputted relevant student data

--- src/pages/students/students.jsx
+++ src/pages/students/students.jsx
@@ -3,9 +3,16 @@ import students from "../../assets/data/student-data";
 
 
 const Students = () => {
-    const studentData = students.map(student => {
-        return student.first_name;
-    })
+    const studentData = students.map(student => (
+        <div>
+            <p>{student.first_name} {student.last_name}</p>
+            <p>{student.email}</p>
+            <p>{student.mobile_number}</p>
+            <p>{student.type}</p>
+            <p>{student.employed}</p>
+            <p>{student.course_name}</p>
+        </div>
+    ))
 
     return (
         <div>
