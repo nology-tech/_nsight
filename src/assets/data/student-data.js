d59ab82df17f7f8b83cd9213e2fd8a7604f51234
Sarah Holland <sholland236@gmail.com>
2021-09-21 15:29:07 +0100

    data set created

---
+++ src/assets/data/student-data.js
@@ -0,0 +1,122 @@
+const students = [
+    {
+        name: "Marion Little",
+        email: "amard53@gmail.com",
+        mobile_number: "536 319 2328",
+        type: "consumer",
+        employed: true,
+        course_name: "Mariana"
+    },
+    {
+        name: "Wm Heaney",
+        email: "heaney50@gmail.com",
+        mobile_number: "+441234567890",
+        type: "consultant",
+        employed: true,
+        course_name: "Aruba"
+    },
+    {
+        name: "Louis Thiel",
+        email: "thiel90@hotmail.com",
+        mobile_number: "+441234567890",
+        type: "consultant",
+        employed: false,
+        course_name: "Jersey"
+    },
+    {
+        name: "Luke Mueller",
+        email: "luke75@gmail.com",
+        mobile_number: "+441234567890",
+        type: "consumer",
+        employed: true,
+        course_name: "Mariana"
+    },
+    {
+        name: "Lee Volkman",
+        email: "volk54@gmail.com",
+        mobile_number: "+441234567890",
+        type: "consultant",
+        employed: true,
+        course_name: "Citibank"
+    },
+    {
+        name: "Molly Hayes",
+        email: "mollyH@gmail.com",
+        mobile_number: "+441234567890",
+        type: "consumer",
+        employed: true,
+        course_name: "Self-paced"
+    },
+    {
+        name: "Melba Dicki",
+        email: "melba@gmail.com",
+        mobile_number: "+441234567890",
+        type: "consumer",
+        employed: true,
+        course_name: "Self-paced"
+    },
+    {
+        name: "Dianne Lang",
+        email: "dlang7272@hotmail.com",
+        mobile_number: "+441234567890",
+        type: "consultant",
+        employed: true,
+        course_name: "Self-paced"
+    },
+    {
+        name: "Mary Bogan",
+        email: "mary1@hotmail.com",
+        mobile_number: "+441234567890",
+        type: "consultant",
+        employed: true,
+        course_name: "Deutsche Bank"
+    },
+    {
+        name: "Marion Little",
+        email: "amard53@gmail.com",
+        mobile_number: "536 319 2328",
+        type: "consumer",
+        employed: true,
+        course_name: "Mariana"
+    },
+    {
+        name: "Wm Heaney",
+        email: "heaney50@gmail.com",
+        mobile_number: "+441234567890",
+        type: "consultant",
+        employed: true,
+        course_name: "Aruba"
+    },
+    {
+        name: "Louis Thiel",
+        email: "thiel90@hotmail.com",
+        mobile_number: "+441234567890",
+        type: "consultant",
+        employed: false,
+        course_name: "Jersey"
+    },
+    {
+        name: "Luke Mueller",
+        email: "luke75@gmail.com",
+        mobile_number: "+441234567890",
+        type: "consumer",
+        employed: true,
+        course_name: "Mariana"
+    },
+    {
+        name: "Lee Volkman",
+        email: "volk54@gmail.com",
+        mobile_number: "+441234567890",
+        type: "consultant",
+        employed: true,
+        course_name: "Citibank"
+    },
+    {
+        name: "Molly Hayes",
+        email: "mollyH@gmail.com",
+        mobile_number: "+441234567890",
+        type: "consumer",
+        employed: true,
+        course_name: "Self-paced"
+    }
+]
