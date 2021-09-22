ca9d3b74f79b4aa80a87c551eb55013d028ada4c
Alex Wower <woweralexander@gmail.com>
2021-09-22 15:13:36 +0100

    added colors to _variable.scss for navbar

--- src/components/navbar/navbar.jsx
+++ src/components/navbar/navbar.jsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import "./navbar.scss";
-import "../../assets/styles/_variables.scss"
 import book from "../../assets/icons/book.svg"
 import medals from "../../assets/icons/medals.svg"
 import nut from "../../assets/icons/nut.svg"
@@ -12,26 +11,28 @@ const Navbar = () => {
     return (
         <nav className="navbar">
             <h1 className="navbar_header">_n</h1>
-            <div className="navbar__item">
-                <img src={house} alt="" />
-                <p>Dashboard</p>
+            <div className="row">
+                <div className="navbar__item">
+                    <img src={house} alt="" className="" />
+                    <p>Dashboard</p>
+                </div>
+                <div className="navbar__item">
+                    <img src={book} alt="" />
+                    <p>Courses</p>
+                </div>
+                <div className="navbar__item">
+                    <img src={students} alt="" />
+                    <p>Students</p>
+                </div>
+                <div className="navbar__item">
+                    <img src={medals} alt="" />
+                    <p>Enrollment</p>
+                </div>
+                <div className="navbar__item">
+                    <img src={nut} alt="" />
+                    <p>Settings</p>
+                </div>     
             </div>
-            <div className="navbar__item">
-                <img src={book} alt="" />
-                <p>Courses</p>
-            </div>
-            <div className="navbar__item">
-                <img src={students} alt="" />
-                <p>Students</p>
-            </div>
-            <div className="navbar__item">
-                <img src={medals} alt="" />
-                <p>Enrollment</p>
-            </div>
-            <div className="navbar__item">
-                <img src={nut} alt="" />
-                <p>Settings</p>
-            </div>     
         </nav>
     )
 }
