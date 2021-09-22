b721794fe52ab877af4e2246da557ebd6048d2eb
Alex Wower <woweralexander@gmail.com>
2021-09-22 16:16:21 +0100

    navbar with styling issues

--- src/components/navbar/navbar.jsx
+++ src/components/navbar/navbar.jsx
@@ -9,30 +9,28 @@ import house from "../../assets/icons/house.svg"
 
 const Navbar = () => {
     return (
-        <nav className="navbar">
+        <nav className="navbar container">
             <h1 className="navbar_header">_n</h1>
-            <div className="row">
-                <div className="navbar__item">
-                    <img src={house} alt="" className="" />
-                    <p>Dashboard</p>
-                </div>
-                <div className="navbar__item">
-                    <img src={book} alt="" />
-                    <p>Courses</p>
-                </div>
-                <div className="navbar__item">
-                    <img src={students} alt="" />
-                    <p>Students</p>
-                </div>
-                <div className="navbar__item">
-                    <img src={medals} alt="" />
-                    <p>Enrollment</p>
-                </div>
-                <div className="navbar__item">
-                    <img src={nut} alt="" />
-                    <p>Settings</p>
-                </div>     
+            <div className="row navbar__item justify-content-start">
+                <a html="https://www.google.com" className="col"><img src={house} alt="" />Dashboard</a>
+                {/* <p className="col">Dashboard</p> */}
             </div>
+            <div className="row navbar__item justify-content-start">
+                <a html="#" className="col"><img src={book} alt="" className="col" />Courses</a>
+                {/* <p className="col">Courses</p> */}
+            </div>
+            <div className="row navbar__item justify-content-start" >
+                <img src={students} alt="" className="col" />
+                <p className="col">Students</p>
+            </div>
+            <div className="row navbar__item">
+                <img src={medals} alt="" className="col" />
+                <p className="col">Enrollment</p>
+            </div>
+            <div className="row navbar__item">
+                <img src={nut} alt="" className="col" />
+                <p className="col">Settings</p>
+            </div>     
         </nav>
     )
 }
