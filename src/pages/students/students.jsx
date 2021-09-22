e0c6fe2c2e8463e87ddcec3f1a1978de6d6caacf
jeromekithinji <kithinjijerome@gmail.com>
2021-09-22 16:42:26 +0100

    Search component added

--- src/pages/students/students.jsx
+++ src/pages/students/students.jsx
@@ -1,14 +1,37 @@
 import "./students.scss";
 import students from "../../assets/data/student-data";
 import moreInfo from "../../assets/icons/chevron-more.svg";
+import Searchbox from "../../components/searchbox/searchbox";
+import { useState } from "react";
 
 
 const Students = () => {
+
+    const [searchTerm, setSearchTerm] = useState("");
+
+    // key in search term
+    // onchange 
+    // sanitize input and data file (same casing)
+    // split the search term for the first and last name
+    // check through the data for the first name, last name given the search term
+    // display relevant student
+
+    const handleSearch = (e) => {
+        const sanitiseInput = e.target.value.toLowerCase();
+        const studentSearch = students.filter(student => {
+            const sanitisedStudentName = student.first_name.toLowerCase();
+            return sanitisedStudentName.includes(sanitiseInput);
+        })
+        setSearchTerm(studentSearch)
+    }
+
+
     const studentData = students.map(student => {
         let employed = "NO";
         if (student.employed) {
             employed = "YES"
-        }
+    }
+
             
         return <div class="row">
                     <div class="col-lg">
@@ -37,6 +60,10 @@ const Students = () => {
 
     return (
         <>
+            <div className="list-title">
+                <h3 className="list-title__heading">Student List</h3>
+                <Searchbox handleSearch={handleSearch} />
+            </div>
             <div class="container">
                 <div class="row">
                     <div class="col-lg">
