1affadb5eba77213244a16effdd634f508f3ec19
Alex Wower <woweralexander@gmail.com>
2021-09-22 21:19:55 +0100

    navbar styling on hover added+ border color/radius

--- src/components/navbar/navbar.jsx
+++ src/components/navbar/navbar.jsx
@@ -12,26 +12,26 @@ const Navbar = () => {
         <nav className="navbar container">
             <h1 className="navbar_header">_n</h1>
             <div className="row navbar__item justify-content-start">
-                <a html="https://www.google.com" className="col align-items-left"><img src={house} alt="" />Dashboard</a>
+                <a html="https://www.google.com" className="col"><img src={house} alt="" />Dashboard</a>
                 {/* <p className="col">Dashboard</p> */}
             </div>
             <div className="row navbar__item justify-content-start">
-                <a html="#" className="col align-items-left"><img src={book} alt="" className="smaller-image" />Courses</a>
+                <a html="#" className="col"><img src={book} alt="" className="smaller-image" />Courses</a>
                 {/* <p className="col">Courses</p> */}
             </div>
             <div className="row navbar__item justify-content-start" >
-                <a html="#" className="col align-items-left"><img src={students} alt="" className="" />Students</a>
+                <a html="#" className="col"><img src={students} alt="" className="" />Students</a>
                 {/* <img src={students} alt="" className="col-md-3" />
                 <p className="col-md-9">Students</p> */}
             </div>
             <div className="row navbar__item text-left">
-                <a html="#" className="col align-items-left"><img src={medals} alt="" className="smaller-image" />Enrollment</a>
+                <a html="#" className="col"><img src={medals} alt="" className="smaller-image" />Enrollment</a>
                 {/* <img src={medals} alt="" className="col-md-3" />
                 <p className="col-md-9">Enrollment</p> */}
             </div>
             <div className="list-break-bottom"></div>
             <div className="row navbar__item text-left">
-                <a html="#" className="col align-items-left"><img src={nut} alt="" className="smaller-image" />Settings</a>
+                <a html="#" className="col"><img src={nut} alt="" className="smaller-image" />Settings</a>
                 {/* <img src={nut} alt="" className="col-md-4" />
                 <p className="col-md-8">Settings</p> */}
             </div>     
