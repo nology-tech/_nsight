62625ca4fa2a5ce70439f0f54e16f4264fa3ac2f
khorh <helen.khor@googlemail.com>
2021-09-23 15:24:02 +0100

    added sort functionality

--- src/pages/students/students.jsx
+++ src/pages/students/students.jsx
@@ -1,12 +1,14 @@
 import { useState, useEffect } from "react";
 import "./students.scss";
 import Searchbox from "../../components/searchbox/searchbox";
+import Sort from "../../components/sort/sort";
 import StudentList from "../../components/studentList/studentList";
 import studentsData from "../../assets/data/student-data";
 
 const Students = () => {
     const [students, setStudents] = useState([]);
 
+    // Search by first and last name
     const handleSearch = (e) => {
         const sanitiseInput = e.target.value.toLowerCase();
         const studentSearch = studentsData.filter(student => {
@@ -16,6 +18,37 @@ const Students = () => {
             return sanitisedStudentName.includes(sanitiseInput);
         })
         setStudents(studentSearch)
+    };
+
+    // Sort by first and last name
+    const sortAscendingAZ = (a, b) => {
+        if (a.first_name < b.first_name) {
+            return -1;
+        } else if (a.first_name > b.first_name) {
+            return 1;
+        } else {
+            return 0;
+        }
+    };
+
+    const sortAscendingByFirstName = () => {
+        const studentsSortedByName = students.filter(student => student).sort(sortAscendingAZ);
+        setStudents(studentsSortedByName);
+    };
+
+    const sortDescendingAZ = (a, b) => {
+        if (b.first_name < a.first_name) {
+            return -1;
+        } else if (b.first_name > a.first_name) {
+            return 1;
+        } else {
+            return 0;
+        }
+    };
+
+    const sortDescendingByFirstName = () => {
+        const studentsSortedByName = students.filter(student => student).sort(sortDescendingAZ);
+        setStudents(studentsSortedByName);
     };
 
     const getStudents = () => {
@@ -31,6 +64,7 @@ const Students = () => {
             <div className="list-title">
                 <h3 className="list-title__heading">Student List</h3>
                 <Searchbox handleSearch={handleSearch} />
+                <Sort sortAscendingByFirstName={sortAscendingByFirstName} sortDescendingByFirstName={sortDescendingByFirstName} />
             </div>
             <StudentList studentData={students} />
         </>
