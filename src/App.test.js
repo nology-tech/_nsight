ce7849fd3e99d7205bf6e94a723397e34ed5760f
joaquinginzo <joaquinginzo@gmail.com>
2021-09-30 17:07:26 +0100

    btns tests updated with new classes

--- src/App.test.js
+++
@@ -1,6 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
-import userEvent from "@testing-library/user-event";
-
-//Using render method -> tells us that we want to work with the functionality of that specific component.
-
