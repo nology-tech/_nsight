340e53ba0d92f47dda0ba2d94f439bd3fce1637f
Sarah Holland <sholland236@gmail.com>
2021-10-05 15:06:07 +0100

    pull fix

--- src/components/sort/sort.jsx
+++ src/components/sort/sort.jsx
@@ -1,16 +1,26 @@
 import "./sort.scss";
 import sortIcon from "../../assets/icons/sort.svg";
+import { useState } from "react";
 
 const Sort = (props) => {
     const { sortAscendingByFirstName, sortDescendingByFirstName } = props;
-
+    const [sort, setSort] = useState(false);
+    const toggleSort = () => {
+        setSort(!sort)
+    }
     return (
-        <>
-            <img src={sortIcon} alt="Sort icon" />
-            <button onClick={sortAscendingByFirstName}>Sort A-Z</button>
-            <button onClick={sortDescendingByFirstName}>Sort Z-A</button>
-        </>
+        <div>
+            <div onClick={toggleSort} className="sort">
+                <img src={sortIcon} alt="Sort icon" />
+                <p>Sort</p>
+            </div>
+            {sort &&
+                <div>
+                    <p onClick={sortAscendingByFirstName}>Sort A-Z</p>
+                    <p onClick={sortDescendingByFirstName}>Sort Z-A</p>
+                </div>
+            }
+        </div>
     );
 };
-
 export default Sort;
