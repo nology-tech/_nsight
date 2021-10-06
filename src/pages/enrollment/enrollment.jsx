53623e089d53bdea7575af8ca3b2050f13b21000
jeromekithinji <kithinjijerome@gmail.com>
2021-10-06 17:31:59 +0100

    Updated styling for header

--- src/pages/enrollment/enrollment.jsx
+++ src/pages/enrollment/enrollment.jsx
@@ -2,12 +2,16 @@ import React from 'react'
 import './enrollment.scss'
 import enrollmentData from "../../assets/data/enrollment-data";
 import EnrollmentList from "../../components/enrollmentList/enrollmentList"
+import TopHeader from '../../components/topheader/topheader';
 
 const Enrollment = () => {
 
     return (
-        <div className="enrollment-list">
-            <EnrollmentList enrollmentData={enrollmentData} />
+        <div className="enrollment-container">
+            <TopHeader title="Enrollments"/>
+            <div className="enrollment-list">
+                <EnrollmentList enrollmentData={enrollmentData} />
+            </div>
         </div>
     )
 }
