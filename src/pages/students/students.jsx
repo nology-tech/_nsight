081ed93c5f6b370a828bd0eef57420619e490e10
Sarah Holland <sholland236@gmail.com>
2021-10-13 11:50:46 +0100

    remove comment

--- src/pages/students/students.jsx
+++ src/pages/students/students.jsx
@@ -261,12 +261,6 @@ const Students = () => {
                                     </li>
                                 </ul>
                             </div>
-                        //     <select name="cars" id="cars">
-                        //     <option value="5">5</option>
-                        //     <option value="10">10</option>
-                        //     <option value="15">15</option>
-                        //     <option value="20">20</option>
-                        // </select>
                         )}
                     </div>
                 <div className="show-results">
