5b736f822401c83984c80606a11eca63fd9741e9
Andy Evans <andy@nology.io>
2021-10-13 12:07:17 +0100

    repository restructure

---
+++ backend/src/main/java/com/nology/nsight/NsightApplication.java
@@ -0,0 +1,13 @@
+package com.nology.nsight;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NsightApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NsightApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/nology/nsight/NsightApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nology.nsight;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NsightApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
