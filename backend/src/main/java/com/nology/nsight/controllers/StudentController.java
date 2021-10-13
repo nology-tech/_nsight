03cab166de9ec233e6d63d72e725ac69bde0511c
Alex Wower <woweralexander@gmail.com>
2021-10-13 15:21:00 +0100

    added POST and Get by id

--- backend/src/main/java/com/nology/nsight/controllers/StudentController.java
+++ backend/src/main/java/com/nology/nsight/controllers/StudentController.java
@@ -3,8 +3,7 @@ package com.nology.nsight.controllers;
 import com.nology.nsight.entities.Students;
 import com.nology.nsight.repositories.IStudentRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -21,8 +20,16 @@ public class StudentController {
     }
 
     // GET/{id}
+    @GetMapping("/students/{id}")
+    public Students getStudentById(@PathVariable int id) {
+        return repository.findById(id).get();
+    }
 
     // POST
+    @PostMapping("/students")
+    public Students createStudent(@RequestBody Students newStudent) {
+        return repository.save(newStudent);
+    }
 
     // PUT
 
