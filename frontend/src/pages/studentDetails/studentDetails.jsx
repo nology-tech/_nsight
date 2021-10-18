35b772291586cea77309a54f235b075c59386ff3
jeromekithinji <kithinjijerome@gmail.com>
2021-10-18 17:02:53 +0100

    Student page component

---
+++ frontend/src/pages/studentDetails/studentDetails.jsx
@@ -0,0 +1,30 @@
+import React, { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+import StudentIndex from "../students/studentIndex/studentIndex";
+import TopHeader from "../../components/topheader/topheader";
+import "./studentDetails.scss";
+
+const StudentDetails = (props) => {
+    const { studentId } = useParams();
+    const [student, setStudent] = useState("");
+
+    const fetchStudents = async () => {
+        const response = await fetch(`http://localhost:8080/students/${studentId}`);
+        const jsonResponse = await response.json();
+        console.log(jsonResponse);
+        return jsonResponse;
+    };
+
+    useEffect(async () => {
+        setStudent(await fetchStudents());
+    }, []);
+
+    return (
+<div className="main-student">
+            <TopHeader title={`${student.first_name} ${student.last_name}`} />
+            <StudentIndex />
+        </div>
+    );
+};
+
+export default StudentDetails;
